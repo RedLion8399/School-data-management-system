258a447c3b6febb98a08e3c1bdcca6d66850d9d9
Paul Jonas <Paul.Jonas@dohle-net.de>
2025-09-01 20:41:31 +0200

    feat: Ann table to add subjects
    - Format and Layout table components
    - Add functions to autoformat the table
    - If all rows are empts a new one is added from a template and if one is empts (exept for the last one) it is deleted.

--- script/main.js
+++ script/main.js
@@ -2,6 +2,17 @@ var settingsOpen;
 window.onload = function () {
     settingsOpen = false;
 };
+/**
+ * Toggle the settings menu on and off.
+ *
+ * When the settings menu is open, the icon is set to a cross and the
+ * settings menu is displayed as a block. The icon is positioned at the
+ * top left of the window.
+ *
+ * When the settings menu is closed, the icon is set to a gear and the
+ * settings menu is not displayed. The icon is positioned statically
+ * in the regular flow of the page.
+ */
 function toggleSettings() {
     var settingsIcon = document.getElementById("settings-icon");
     var settings = document.getElementById("settings-container");
@@ -20,7 +31,72 @@ function toggleSettings() {
         settingsIcon.classList.add("fa-xmark");
         settings.style.display = "block";
         settingsIcon.style.position = "fixed";
-        settingsIcon.style.top = "12px";
+        settingsIcon.style.top = "25px";
         settingsIcon.style.left = "10px";
+        if (isRowEmpty()) {
+            addRow();
+        }
     }
+}
+var subjectTable = document.getElementById("subject-selection");
+subjectTable.addEventListener("input", function (event) {
+    var target = event.target;
+    var row = target.closest("tr");
+    addSubject(row);
+    if (isRowEmpty()) {
+        addRow();
+    }
+    deleteUnusedRows();
+});
+function addSubject(self) {
+    var subjectName;
+    var subjectColor;
+}
+/**
+ * Checks if all cells in all rows of the table with the id "subject-selection"
+ * are filled.
+ *
+ * @returns {boolean} True if all cells are filled, false otherwise
+ */
+function isRowEmpty() {
+    var table = document.getElementById("subject-selection");
+    var rows = table.rows;
+    for (var i = 0; i < rows.length; i++) {
+        var cells = rows[i].cells;
+        for (var j = 0; j < cells.length; j++) {
+            if (cells[j].innerHTML == "") {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+/**
+ * Deletes all empty rows from the subject setting table and deletes them.
+ * The last row is not deleted but all rows in between.
+ */
+function deleteUnusedRows() {
+    var table = document.getElementById("subject-selection");
+    var rows = table.rows;
+    for (var i = 0; i < rows.length; i++) {
+        var cells = rows[i].cells;
+        if (cells[0].innerHTML != "<br>") {
+            continue;
+        }
+        if (i == rows.length - 1) {
+            continue;
+        }
+        table.deleteRow(i);
+    }
+}
+/**
+ * Adds a new row to the table with the id "subject-selection".
+ * The row is copied from the template with the id "subject-template"
+ * and added to the bottom of the table.
+ */
+function addRow() {
+    var table = document.getElementById("subject-selection");
+    var template = document.getElementById("subject-template");
+    var templateClone = template.content.cloneNode(true);
+    table.appendChild(templateClone);
 }
--- script/main.ts
+++ script/main.ts
@@ -4,6 +4,17 @@ window.onload = () => {
   settingsOpen = false;
 };
 
+/**
+ * Toggle the settings menu on and off.
+ *
+ * When the settings menu is open, the icon is set to a cross and the
+ * settings menu is displayed as a block. The icon is positioned at the
+ * top left of the window.
+ *
+ * When the settings menu is closed, the icon is set to a gear and the
+ * settings menu is not displayed. The icon is positioned statically
+ * in the regular flow of the page.
+ */
 function toggleSettings() {
   const settingsIcon = document.getElementById("settings-icon")!;
   const settings = document.getElementById("settings-container")!;
@@ -23,7 +34,84 @@ function toggleSettings() {
     settings.style.display = "block";
 
     settingsIcon.style.position = "fixed";
-    settingsIcon.style.top = "12px";
+    settingsIcon.style.top = "25px";
     settingsIcon.style.left = "10px";
+
+    if (isRowEmpty()) {
+      addRow();
+    }
   }
+}
+
+let subjectTable = document.getElementById("subject-selection")!;
+subjectTable.addEventListener("input", (event) => {
+  const target = event.target as HTMLInputElement;
+  const row = target.closest("tr") as HTMLTableRowElement;
+  addSubject(row);
+  if (isRowEmpty()) {
+    addRow();
+  }
+  deleteUnusedRows();
+});
+
+function addSubject(self: HTMLElement) {
+  let subjectName;
+  let subjectColor;
+}
+
+/**
+ * Checks if all cells in all rows of the table with the id "subject-selection"
+ * are filled.
+ *
+ * @returns {boolean} True if all cells are filled, false otherwise
+ */
+function isRowEmpty() {
+  let table: HTMLTableElement = document.getElementById(
+    "subject-selection"
+  ) as HTMLTableElement;
+  let rows: HTMLCollectionOf<HTMLTableRowElement> = table.rows;
+  for (let i = 0; i < rows.length; i++) {
+    let cells = rows[i].cells;
+    for (let j = 0; j < cells.length; j++) {
+      if (cells[j].innerHTML == "") {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+/**
+ * Deletes all empty rows from the subject setting table and deletes them.
+ * The last row is not deleted but all rows in between.
+ */
+function deleteUnusedRows() {
+  let table: HTMLTableElement = document.getElementById(
+    "subject-selection"
+  ) as HTMLTableElement;
+  let rows: HTMLCollectionOf<HTMLTableRowElement> = table.rows;
+  for (let i = 0; i < rows.length; i++) {
+    let cells: HTMLCollectionOf<HTMLTableCellElement> = rows[i].cells;
+    if (cells[0].innerHTML != "<br>") {
+      continue;
+    }
+    if (i == rows.length - 1) {
+      continue;
+    }
+    table.deleteRow(i);
+  }
+}
+
+/**
+ * Adds a new row to the table with the id "subject-selection".
+ * The row is copied from the template with the id "subject-template"
+ * and added to the bottom of the table.
+ */
+function addRow() {
+  let table = document.getElementById("subject-selection") as HTMLTableElement;
+  let template: HTMLTemplateElement = document.getElementById(
+    "subject-template"
+  ) as HTMLTemplateElement;
+  let templateClone: Node = template.content.cloneNode(true);
+  table.appendChild(templateClone);
 }
